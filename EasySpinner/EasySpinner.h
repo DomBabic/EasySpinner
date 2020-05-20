34bf8681ccca03c065de5fa065b781adcc722306
Dominik Babić <domynick93@hotmail.com>
2020-05-20 14:32:42 +0200

    Initial commit.

---
+++ EasySpinner/EasySpinner.h
@@ -0,0 +1,19 @@
+//
+//  EasySpinner.h
+//  EasySpinner
+//
+//  Created by Dominik Babić on 20/05/2020.
+//  Copyright © 2020 HeliOs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasySpinner.
+FOUNDATION_EXPORT double EasySpinnerVersionNumber;
+
+//! Project version string for EasySpinner.
+FOUNDATION_EXPORT const unsigned char EasySpinnerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasySpinner/PublicHeader.h>
+
+
